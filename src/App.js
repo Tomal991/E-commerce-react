c2c47665256c00f4fa0b0ba4d9d7488c52198612
Tomal991 <intesarulhaquey4@gmail.com>
2021-01-12 18:29:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="#">E-Cart</a>
+        </div>
+        <div>
+          <a href="#">Home</a>
+          <a href="#">Products</a>
+          <a href="#">About</a>
+          <a href="#">Contacts</a>
+        </div>
       </header>
+      <main>
+        <div>
+          <div className="row center">
+            {
+              data.products.map(product => (
+                <div key={product._id} className="card">
+                  <a href={'/product/${product._id}'}>
+
+                    <img className="medium" src={product.image} alt={product.type} />
+                  </a>
+                  <div className="card-body">
+                  <a href={'/product/${product._id}'}>
+                      <h2>{product.type}</h2>
+                    </a>
+                    <div className="rating">
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                    </div>
+                    <div className="price">{product.price}tk</div>
+                  </div>
+                </div>
+              ))
+            }
+
+
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            type: 'Casual Shirt',
+            catagory: 'Shirt',
+            price: 500,
+            image: './images/4.jpg',
+            rating: 3.5,
+            numofReviews: 10,
+
+        },
+        {
+            _id: '2',
+            type: 'Casual Shirt',
+            catagory: 'Shirt',
+            price: 500,
+            image: './images/5.jpg',
+            rating: 5.00,
+            numofReviews: 5,
+
+        },
+        {
+            _id: '3',
+            type: 'Formal Shirt',
+            catagory: 'Shirt',
+            price: 1000,
+            image: './images/2.jpg',
+            rating: 4.3,
+            numofReviews: 15,
+
+        },
+        {
+            _id: '4',
+            type: 'Formal Shirt',
+            catagory: 'Shirt',
+            price: 1000,
+            image: './images/3.jpg',
+            rating: 4.5,
+            numofReviews: 10,
+
+        },
+        {
+            _id: '5',
+            type: 'Casual Shirt',
+            catagory: 'Shirt',
+            price: 500,
+            image: './images/4.jpg',
+            rating: 5.00,
+            numofReviews: 20,
+
+        },
+        {
+            _id: '6',
+            type: 'Formal Shirt',
+            catagory: 'Shirt',
+            price: 1000,
+            image: './images/2.jpg',
+            rating: 4.00,
+            numofReviews: 10,
+
+        },
+    ],
+};
+
+export default data;
